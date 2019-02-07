e53ade8f8a3fba03978ed943b37a550fb163e70f
Clement Gohlke <Clement@test.fritz.box>
2019-02-07 16:39:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="OnClickCoin">
+        Welcome to OnClickCoin!
+      </div>
+    );
+  }
+}
+
+export default App;
