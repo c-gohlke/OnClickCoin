19c61574ddca5a74c0fb122e27a58a42f117b956
Clement Gohlke <Clement@test.fritz.box>
2019-02-07 23:21:13 +0100

    dropdown real shit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,54 +1,15 @@
-import React, { Component } from 'react';
-import Dropdown from '../components/Dropdown'
-
+import React, { Component } from "react";
 
 class Home extends Component {
-  constructor(){
-    super()
-      this.state = {
-          location: [
-              {
-                  id: 0,
-                  title: "NYC",
-                  key: 'location'
-
-              },
-              {
-                  id: 1,
-                  title: "BER",
-                  key: 'location'
-
-              },
-              {
-                id: 2,
-                title: "MUN",
-                key: 'location'
-              }
-          ]
-
-      }
-  }
-
-    toggleSelected(id, key){
-        let temp = this.state[key]
-        temp[id].selected = !temp[id].selected
-        this.setState({
-            [key]: temp
-        })
-    }
-
-
   render() {
     return (
-
-      <div className="OnClickCoin">
-        Welcome to OnClickCoin!
-          <Dropdown
-              title="Click to select chain"
-              list={this.state.location}
-              toggleItem={this.toggleSelected}/>
+      <div>
+        <div className="OnClickCoin">Welcome to OnClickCoin!</div>
+        <select id="network">
+          <option value="0" defaultChecked>rynkeby</option>
+          <option value="1">kovan</option>
+        </select>
       </div>
-
     );
   }
 }
--- src/utils/buildDeployment.js
+++ src/utils/buildDeployment.js
@@ -1,17 +1,20 @@
-import {endpoint, sendAddress, gasPrice, gasLimit, chainId, networkID} from '../config/config'
+import {endpoint, sendAddress, gasPrice, gasLimit, chainId} from '../config/config'
 import bytecode from "../contracts/basicContract"
 const Web3 = require("web3");
-const web3 = new Web3(new Web3.providers.HttpProvider(endpoint[networkID]));
-
-const bcode =
-  "0x" + bytecode;
 
 async function buildDeployment() {
+  var netID = document.getElementById("network");
+  var network = netID.options[netID.selectedIndex].value;
+  const web3 = new Web3(new Web3.providers.HttpProvider(endpoint[network]));
+
+  const bcode =
+  "0x" + bytecode;
+
   var nonce = await web3.eth.getTransactionCount(sendAddress, 'pending');
   var gasP = web3.utils.toHex(gasPrice);
   var gasL = web3.utils.toHex(gasLimit);
   var value = web3.utils.toHex(web3.utils.toWei("0", "ether"));
-  var cId = web3.utils.toHex(chainId[networkID]);
+  var cId = web3.utils.toHex(chainId[network]);
   var data = bcode;
 
   console.log('new transaction nonce is', nonce)
