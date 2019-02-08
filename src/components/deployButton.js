e60b698809a268153f0505b737a9f410dc3677af
Clement Gohlke <Clement@test.fritz.box>
2019-02-08 22:02:07 +0100

    deployment through metamask

--- src/components/deployButton.js
+++ src/components/deployButton.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import deployContract from "../utils/deployContract";
+import deployMetaContract from "../utils/deployMetaContract";
 
 class DeployButton extends Component {
   handleClick(event) {
-    deployContract();
+    const payerList = document.getElementById("paymentMethod");
+    var payer = parseInt(payerList.options[payerList.selectedIndex].value);
+    console.log("payer is", payer)
+    if(payer===0){
+      deployContract();
+    }
+    else if(payer===1){
+      deployMetaContract()
+    }
+    else{
+      console.log("payer value problem")
+    }
   }
 
   render() {
