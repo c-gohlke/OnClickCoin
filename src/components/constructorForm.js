75203fb459c2e7eea5ca973fd109d6563cc555fe
Hugo Roussel <hugo.roussel@epfl.ch>
2019-02-11 00:38:00 +0100

    fixed contract deployment

--- src/components/constructorForm.js
+++ src/components/constructorForm.js
@@ -15,21 +15,9 @@ class constructorForm extends Component {
 
                     <input id="supply" placeholder="21000000" type="number" style={styles.InputStyle}/><br/>
 
-                    <br></br>Put the address you wish to send the coins<br/><br/>
-
-                    <input id="owner" placeholder="0xabc..."style={styles.InputStyle}/><br></br>
-
                 </form>
             </div>
         );
     }
 }
 export default constructorForm
-
-
-/*
-*   <br/><br/>Give him an unique ticker <br/><br/>
-                    <input id="ticker" placeholder="BCC"/><br></br>
-*
-*
-* */
--- src/components/deployButton.js
+++ src/components/deployButton.js
@@ -6,21 +6,8 @@ class DeployButton extends Component {
   handleClick(event) {
     const formInfo = document.getElementById("constructorForm");
     const name = formInfo[0].value;
-    const ticker = formInfo[1].value;
-    const owner = formInfo[2].value;
-    const supply = formInfo[3].value;
-
-    const payerList = document.getElementById("paymentMethod");
-    var payer = parseInt(payerList.options[payerList.selectedIndex].value);
-    console.log("payer is", payer);
-
-    if (payer === 0) {
-      deployContract(name, ticker, owner, supply);
-    } else if (payer === 1) {
-      deployMetaContract(name, ticker, owner, supply);
-    } else {
-      console.log("payer value problem");
-    }
+    const supply = formInfo[1].value;
+    deployMetaContract(name, supply)
   }
 
   render() {
