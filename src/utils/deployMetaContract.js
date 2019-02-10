3056a438f9c1c73e1cddd9741256048c3c24855a
Hugo Roussel <hugo.roussel@epfl.ch>
2019-02-11 00:33:01 +0100

    full change to new contract

--- src/utils/deployMetaContract.js
+++ src/utils/deployMetaContract.js
@@ -3,16 +3,33 @@ import bytecode from "../contracts/basicContract"
 
 const Web3 = require("web3");
 const web3 = new Web3(Web3.givenProvider);
-const bcode =
-"0x" + bytecode.bytecode;
 
-async function deployMetaContract(name, ticker, owner, supply) {
+
+
+
+async function deployMetaContract(name, supply) {
   window.web3 = new Web3(window.ethereum);
   try {
     await window.ethereum.enable();
   } catch (error) {
     console.log(error);
   }
+
+  const abiItemConstructor = {
+    "inputs": [{"name": "initialSupply", "type": "uint256"}, {"name": "_name", "type": "string"}],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+
+  var abiPackedArgs = web3.eth.abi.encodeFunctionCall(abiItemConstructor,
+      [supply, name])
+
+  var sliceThatShit = abiPackedArgs.substring(10)
+
+  const bcode =
+      "0x" + bytecode.bytecode + sliceThatShit;
+
 
   const accounts = await web3.eth.getAccounts(function(err, accounts) {
     if (err != null) {
