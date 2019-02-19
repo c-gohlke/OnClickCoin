e10db462c260790e0d57ba81768c42fb86f4af89
Clement Gohlke <Clement@Clem.local>
2019-02-19 19:54:29 +0100

    fix heroku?

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-const express = require('express');
-const path = require('path');
-const port = (process.env.port || 8080);
+const express = require("express");
 const app = express();
 
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
 
-console.log("server.js CALLED!!!")
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get("/", function(req, res) {
+  res.render("src/index.js");
 });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.listen(app.get("port"), function() {
+  console.log("Running on port: ", app.get("port"));
 });
-
-app.listen(port);
