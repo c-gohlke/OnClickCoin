b37411d9643057cd82c57d11095eafc46b750ee7
Clement Gohlke <Clement@Clem.local>
2019-02-19 20:05:18 +0100

    change directory name server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+console.log("server.js called")
+
 app.set("port", process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
 
 // the __dirname is the current directory from where the script is running
 app.get("/", function(req, res) {
-  res.render("src/index.js");
+  res.render(__dirname + "/src/index.js");
+  console.log("rendering ", __dirname + "/src/index.js")
 });
 
 app.listen(app.get("port"), function() {
