d1012d42c4453bd51da27ad64b833a4c5015e1d5
Clement Gohlke <Clement@Clem.local>
2019-02-20 09:47:32 +0100

    change index.js import path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./views/App";
+import App from "app/src/views/App";
 
 ReactDOM.render(<App />, document.getElementById('app'));
