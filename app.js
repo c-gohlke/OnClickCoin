8c603399c6a3f4a99e4e7743fca60e3e93a28512
Clement Gohlke <Clement@Clem.local>
2019-02-20 13:20:54 +0100

    add .bin/www file

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const path = require('path')
-const app = express();
 
+const app = express();
 
 console.log("server.js called")
 
-app.set("port", process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'src/views')); 
+app.set("port", process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'src')); 
 
 app.use(express.static(__dirname + "/public"));
-
-console.log("directory name is, ", __dirname)
-console.log("express views set to ", app.get("views"))
 
 // the __dirname is the current directory from where the script is running
 app.get("/", function(req, res) {
   console.log("rendering ", __dirname + "/src/index.js")
-  res.render(__dirname + "pages/Home.js");
+  res.render(__dirname + "index.js");
 });
 
 app.listen(app.get("port"), function() {
   console.log("Running on port: ", app.get("port"));
 });
+
+module.exports = app
