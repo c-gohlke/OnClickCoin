03c6c4ad0cecfbcf079c9ce396373f24365d2228
Clement Gohlke <Clement@Clem.local>
2019-02-20 13:28:47 +0100

    change bin/www

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path')
 
 const app = express();
 
+const port = normalizePort(process.env.PORT || '3000');
+
 console.log("server.js called")
 
-app.set("port", process.env.PORT || 8080);
+app.set("port", port);
 app.set('views', path.join(__dirname, 'src')); 
 
 app.use(express.static(__dirname + "/public"));
@@ -19,5 +21,22 @@ app.get("/", function(req, res) {
 app.listen(app.get("port"), function() {
   console.log("Running on port: ", app.get("port"));
 });
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 
 module.exports = app
