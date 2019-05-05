999748c7f7e8c2bb2c0f4ac7d018fd1ddf6a8a2c
Clement Gohlke <Clement@Clem.local>
2019-05-05 17:49:22 +0200

    add navigation bar

--- client/components/info/Info.js
+++ client/components/info/Info.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
+import Navbar from "../navbar/Navbar";
 
 /*
 Defines the send page of the app
 */
 
-console.log("in components/info/Info")
+console.log("in components/info/Info");
 
 class Info extends Component {
   render() {
     return (
+      <div>
+        <Navbar />
         <div className="info">
-        < br/>
-        <h1>Info Page :)</h1>
+          <br />
+          <h1>Info Page :)</h1>
+        </div>
       </div>
     );
   }
 }
 
-export default Info
+export default Info;
