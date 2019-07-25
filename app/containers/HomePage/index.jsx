c12c3b07c5a4a1f574577963785574333aa8cb3e
Hugo Roussel <hugo.roussel@epfl.ch>
2019-07-25 23:40:58 +0200

    Add particles

--- app/containers/HomePage/index.jsx
+++ app/containers/HomePage/index.jsx
@@ -3,29 +3,37 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Col from 'react-bootstrap/Col';
 import ContractForm from './ContractForm';
-import coin from '../../images/coins.gif';
+import coin from '../../images/coins_old.gif';
 import Navigationbar from '../../components/Header/Navigationbar';
+import Particles from 'react-particles-js';
+import paramObject from './particlesConfig';
+
 
 /*
 Defines the Homepage of the App
 */
 
+
+
 const Home = () => (
   <div>
+    <Particles params= {paramObject} style={{position: 'absolute'}}/>
     <Navigationbar />
     <Container>
-      <Container style={{ fontFamily: 'Open Sans', textAlign: 'center' }}>
-        <h1>On Click Coin</h1>
-        <h2>Deploy your cryptocurrency token in one click!</h2>
-        <Col>
-          <Container style={{ textAlign: 'center', display: 'inline-block' }}>
-            <Image src={coin} />
-            <Image src={coin} />
-            <Image src={coin} />
-          </Container>
-        </Col>
-        <ContractForm />
+      <Container style={{ fontFamily: 'Open Sans', textAlign: 'center'}}>
+      <h1>On Click Coin</h1>
+      <h2>Deploy your cryptocurrency token in one click!</h2>
+      <br/>
+      <Container style={{ textAlign: 'center', display: 'inline-block' }}>
+          <Image src={coin} />
+          <Image src={coin} />
+          <Image src={coin} />
       </Container>
+      <br/>
+      <br/>
+      <br/>
+      <ContractForm />
+      </Container>      
     </Container>
   </div>
 );
